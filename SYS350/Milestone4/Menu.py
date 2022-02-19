a3c2777a3dfb4d3530b0659b17538b7ce7b4431a
BigMike-Champ <michael.verdi@mymail.champlain.edu>
2022-02-18 19:04:55 -0500

    Update for second assignment

--- SYS350/Milestone4/Menu.py
+++ SYS350/Milestone4/Menu.py
@@ -11,7 +11,15 @@ def menu():
     print("[3] Filter all VMs")
     print("[4] List All VM's")
     print("[5] List Vcenter Information")
-    print("[6] Exit the Program")
+    print("[7] Turn a VM [OFF]")
+    print("[8] Turn a VM [ON]")
+    print("[9] Make a snapshot")
+    print("[10] Delete a VM")
+    print("[11] Modify VM specs")
+    print("[12] Rename a VM")
+    print("[13] Exit the Program")
+    
+    
 
 def opt1():
     with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
@@ -101,10 +109,166 @@ def opt5():
     time.sleep(2)
     print(aboutInfo.osType)
     Disconnect(si)
+
+def opt7():
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+    vm_name=input("Enter a VM name: ")
+    for i in vms:
+        if vm_name == i.name:
+            print("Turning the VM off!")
+            i.PowerOff()
+            time.sleep(5)
+    Disconnect(si)
+def opt8():
+    #https://github.com/vmware/pyvmomi-community-samples/blob/master/samples/vm_power_on.py
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+    vm_name=input("Enter a VM name: ")
+    for i in vms:
+        if vm_name == i.name:
+            print("Turning the VM on!")
+            i.PowerOn()
+            time.sleep(5)
+    Disconnect(si)
+
+def opt9():
+    #https://github.com/vmware/pyvmomi-community-samples/blob/master/samples/create_snapshot.py
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+    vm_name=input("Enter a VM name: ")
+    s_Name=input("Enter what the snapshot should be named")
+    descript=input("Please put in a description")
+    for i in vms:
+        if vm_name == i.name:
+            i.CreateSnapshot_Task(name=s_Name, memory=True, quiesce=False)
+    Disconnect(si)
+def opt10():
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+   #vm_name=input("Enter a VM name: ")
+    name_c=input("Enter the name of the machine to be deleted: ")
+    for i in vms:
+        if name_c == i.name:
+            i.PowerOff()
+            time.sleep(10)
+            print("Deleting the VM")
+            i.Destroy_Task()
+            time.sleep(10)
+    Disconnect(si)
+    13
+
+def opt11():
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+    vm_name=input("Enter a VM name to reconfigure: ")
+    Change_choice=input("Which aspect would you lime to change? CPU or Memory. ")
+    if Change_choice == "CPU":
+        for i in vms:
+            if vm_name == i.name:
+                print("You have chosen to reconfigure the CPU's. Powering the VM off.")
+                i.PowerOff()
+                print("Power is off Proceeding to reconfigure.")
+                number = input("Input the number of CPU's you would like to set: ")
+                time.sleep(2)
+                core = input("Enter the number of Cores you would want: ")
+                time.sleep(2)
+                print("Configuring...")
+                spec = vim.vm.ConfigSpec()
+                spec.numCPUs=int(number)
+                spec.numCoresPerSocket=int(core)
+                i.Reconfigure(spec)
+                print("VM CPU update complete")
+                time.sleep(5)
+            elif Change_choice == "Memory":
+                print("You have chosen to reconfigure the Memory. Powering the VM off.")
+                i.PowerOff()
+                quantity=input("Please enter the quantity memory you would like: ")
+                print("Applying configurations: ")
+                time.sleep(2)
+                numquant=int(quantity)
+                gb=numquant*1024
+                spec = vim.vm.Config(spec)
+                spec.memoryMB = gb
+                i.Reconfigure(gb)
+                print("Configuration Complete...")
+                time.sleep(5)
+    Disconnect(si)
+
+def opt12():
+    with open('/home/champuser/Desktop/Tech_Journal/SYS350/Milestone4/config.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    passw = getpass.getpass()
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    vc = data["vcenter"]
+    un = data["username"]
+    si=SmartConnect(host=vc, user=un, pwd=passw, sslContext=s)
+    si.RetrieveContent()
+    datacenter = si.content.rootFolder.childEntity[0]
+    vms = datacenter.vmFolder.childEntity
+    vm_name=input("Enter a VM name: ")
+    new_name=input("Please enter a name to change the VM name to: ")
+    for i in vms:
+        if vm_name == i.name:
+            print("Renaming Now...")
+            i.Rename(new_name)
+            time.sleep(2)
+            print("Name has been changed")
+            time.sleep(2)
+    Disconnect(si)
+
+
+
 menu()
 option = int(input("Enter your option: "))
 
-while option !=0:
+while option !=0 :
     if option == 1:
         opt1()
     elif option == 2:
@@ -116,10 +280,24 @@ while option !=0:
     elif option == 5:
         opt5()
     elif option == 6:
+        menu2()
+    elif option == 7:
+        opt7()
+    elif option == 8:
+        opt8()
+    elif option == 9:
+        opt9()
+    elif option == 10:
+        opt10()
+    elif option == 11:
+        opt11()
+    elif option == 12:
+        opt12()
+    elif option == 13:
         exit()
     else:
         print("Please input an option")
 
-    print()
+    
     menu()
     option = int(input("Enter your option: "))
